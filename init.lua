4e67b55ee380f0a04106079794f7eede49142019
Zhiyuan Li <zhiyuanli@Zyn-s-MacBook-Air-M3.local>
2024-09-05 15:58:01 -0700

    gitsign

--- init.lua
+++ init.lua
@@ -187,6 +187,7 @@ require('lazy').setup({
   --    require('gitsigns').setup({ ... })
   --
   -- See `:help gitsigns` to understand what the configuration keys do
+  --[[
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -199,7 +200,9 @@ require('lazy').setup({
       },
     },
   },
-
+  ]]
+  --
+  --
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
